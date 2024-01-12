e8710ed3da8d7a0511d12d380dabdf36b108429d
Giovanni-Carrizzo <gcarrizzo19@gmail.com>
2024-01-12 10:26:21 -0800

    Added the ability to move the player

--- myGameApp.py
+++ myGameApp.py
@@ -3,7 +3,7 @@ import pygame as pg
 import random
 
 #parameters
-WIDTH, HEIGHT, FPS = (800,600,30)
+WIDTH, HEIGHT, FPS = (480,600,60)
 #define colours
 WHITE = (255,255,255)
 BLACK = (0,0,0)
@@ -17,18 +17,30 @@ class Player(pg.sprite.Sprite):
     def __init__(self):
         #constructor
         pg.sprite.Sprite.__init__(self) #inheritance
-        self.image = pg.Surface((50,50))
+        self.image = pg.Surface((50,40))
         self.image.fill(GREEN)
         #useful for moving, size, position and collision
         self.rect = self.image.get_rect()  #looks at the image and gets its rect
-        self.rect.center = (WIDTH/2, HEIGHT/2) #places image in the centre
+        self.rect.centerx = WIDTH/2 #places image in the centre
+        self.rect.bottom = HEIGHT-10 #puts it in 10px from bottom of screen
+        #it needs to move side to side so we need speed
+        self.speedx = 0
 
     def update(self):
-        #move the sprite
-        self.rect.x += 5
+        #we will keep the default speed of object to zero and only alter it with a key press
+        #this way we avoid coding for what happens when the key is released
+        self.speedx = 0
+        keystate = pg.key.get_pressed() #returned a list of keys that are down
+        if keystate[pg.K_LEFT]:
+            self.speedx = -5
+        if keystate[pg.K_RIGHT]:
+            self.speedx = 5
+        self.rect.x += self.speedx #move at speed to be set by controls
         #to ensure it does not run off the screen
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
 #Initialise common pygame objects
 pg.init()
 pg.mixer.init()
