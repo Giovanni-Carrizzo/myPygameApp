7f4b535b31fdc2e34245c08052dff948b2c4d533
Giovanni-Carrizzo <gcarrizzo19@gmail.com>
2024-01-15 12:22:12 -0800

    Added graphics as well as fixing the error

--- myGameApp.py
+++ myGameApp.py
@@ -2,8 +2,11 @@
 import pygame as pg
 import random
 
+from os import path
+img_dir = path.join(path.dirname(__file__),'img')#img is the folder where the graphics are
+ 
 #parameters
-WIDTH, HEIGHT, FPS = (480,600,60)
+WIDTH, HEIGHT, FPS = (800,600,60)
 #define colours
 WHITE = (255,255,255)
 BLACK = (0,0,0)
@@ -16,19 +19,14 @@ YELLOW = (255,255,0)
 pg.init()
 pg.mixer.init()
 
-#create the display
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption('My Game')
-clock = pg.time.Clock()
-
 #a sprite will be an object which inherits from the built in sprite class
 class Player(pg.sprite.Sprite):
     #sprite for the player
     def __init__(self):
         #constructor
         pg.sprite.Sprite.__init__(self) #inheritance
-        self.image = pg.Surface((50,40))
-        self.image.fill(GREEN)
+        self.image = pg.transform.scale(player_img,(50,38))
+        self.image.set_colorkey(BLACK)
         #useful for moving, size, position and collision
         self.rect = self.image.get_rect()  #looks at the image and gets its rect
         self.rect.centerx = WIDTH/2 #places image in the centre
@@ -64,8 +62,8 @@ class Mob(pg.sprite.Sprite):
     #enemy mobile object which inherits from the sprite
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((30,40))
-        self.image.fill(RED)
+        self.image = pg.transform.scale(mob_img,(30,40))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         
         #make the enemy spawn off top of screen to appear off thescreen and then start dropping down
@@ -79,14 +77,14 @@ class Mob(pg.sprite.Sprite):
         if self.rect.top > HEIGHT + 10:
             self.rect.x = random.randrange(0, WIDTH - self.rect.width) #appears within the limits of the screen
             self.rect.y = random.randrange(-100,-40) #this is off the screen
-            self.speedy = random.randrange(1,8)
+            self.speedy = random.randrange(1,8)          
 
 class Bullet(pg.sprite.Sprite):
     def __init__(self,x,y):
         #x and y and respawn positions based on the player's position
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.Surface((10,20))
-        self.image.fill(YELLOW)
+        self.image = pg.transform.scale(bullets_img,(30,30))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         #set respawn position to right in front of the player
         self.rect.bottom = y
@@ -99,6 +97,17 @@ class Bullet(pg.sprite.Sprite):
         if self.rect.bottom < 0:
             self.kill()
 
+#create the display
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption('My Game')
+clock = pg.time.Clock()
+
+background = pg.image.load(path.join(img_dir, "bg_1_1.png")).convert()
+background_rect = background.get_rect()
+
+player_img = pg.image.load(path.join(img_dir, "Garley.jpg")).convert()
+mob_img = pg.image.load(path.join(img_dir, "Sean.jpg")).convert()
+bullets_img = pg.image.load(path.join(img_dir, "Lucian.jpg")).convert()
 #create a sprite group
 all_sprites = pg.sprite.Group()
 mobs = pg.sprite.Group()  #creating another group would aid during collision detection
@@ -111,9 +120,11 @@ for i in range(8):
     all_sprites.add(m)
     mobs.add(m)
 
+all_sprites.add(mobs)
 all_sprites.add(player)
 
 #game loop
+
 running = True
 while running:
     #keep the game running at the right speed
@@ -132,20 +143,22 @@ while running:
     all_sprites.update()
     #Check if a bullet hits a mob
     hits = pg.sprite.groupcollide(mobs,bullets,True,True)
+    #respawn mobs destroyed by bullets 
+    for hit in hits:
+        m = Mob()
+        all_sprites.add(m)
+        mobs.add(m)
     #Check to see if a mob hits the player
     hits = pg.sprite.spritecollide(player,mobs,False) #parameters are object to check against and group against
     #False indicates whether hit item in group should be deleted or not
     
-#respawn mobs destroyed by bullets
-for hit in hits:
-    m = Mob()
-    all_sprites.add(m)
-    mobs.add(m)
-
     if hits:
         running = False
     #draw/render
     screen.fill(BLACK)
+    #draw background on screen
+    screen.blit(background,background_rect)
+
     all_sprites.draw(screen)
     #always do this after drawing anything
     pg.display.flip()
